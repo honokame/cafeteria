85152e2e636c9d711d7be17cbef461b0b042c683
okuguchihonoka <e1607@s.akashi.ac.jp>
2020-08-01 21:53:11 +0900

    sql文を小文字にしたらデータの追加ができた

--- review.php
+++ review.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset = "utf-8" />
-  </head>
-  <body>
-    <form action ="review.php" method ="POST">
-      コメント<br />
-      <textarea name = "comment" cols = "50" rows = "5"></textarea>
-      <br />
-      <input type = "submit" name = "send" value = "送信" />
-    </form>
- 
+<html>
+
+<head>
+  <meta charset="utf-8" />
+</head>
+
+<body>
+  <form action="review.php" method="POST">
+    コメント<br />
+    <textarea name="comment" cols="50" rows="5"></textarea>
+    <br />
+    <input type="submit" name="send" value="送信" />
+  </form>
+
   <?php
   $display_enc = "UTF-8";
-  
+
   // データベース接続設定
   $sv = "localhost";
   $name = "team3db";
@@ -22,14 +24,14 @@
 
   // データベースに接続
   $dbconn = pg_connect("host = $sv dbname = $name user = $user password = $pass") or die("接続エラー");
-  //$res = pg_query($dbconn, $sql) or die("データ読み込みエラー")
   if(isset($_POST['send'])){
-    $comment = $_POST["comment"];
-    $sql = "INSERT INTO review(menu,eval,body,date) VALUES('$comment')";
-    $res = pg_query($dbconn,$sql) or die("データ読み込みエラー");
+    // $comment = $_POST["comment"];
+    $sql = "insert into review values('ハンバーグ',5,'umai!!','2020-08-02')";
+    $res = pg_query($dbconn, $sql) or die("データ読み込みエラー");
   }
-  
+
 
   ?>
-  </body>
-  </html>
+</body>
+
+</html>
