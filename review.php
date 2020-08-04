d612180c7448915caf40e11096135abb83e1f4a0
okuguchihonoka <e1607@s.akashi.ac.jp>
2020-08-04 13:32:32 +0900

    cssを別ファイルに

--- review.php
+++ review.php
@@ -2,123 +2,12 @@
 <html>
 
 <head>
+  <link rel="stylesheet" type="text/css" href="review.css" />
   <meta charset="utf-8" />
   <meta name="viewport" content="user-scalable=no" />
 </head>
 
 <body>
-
-  <style type="text/css">
-    /* 星の細かいパラメータ */
-    .evaluation {
-      display: flex;
-      flex-direction: row-reverse;
-      justify-content: center;
-    }
-
-    .evaluation input[type='radio'] {
-      display: none;
-    }
-
-    .evaluation label {
-      position: relative;
-      padding: 20px 20px 0;
-      color: gray;
-      cursor: pointer;
-      font-size: 80px;
-    }
-
-    .evaluation label .text {
-      position: absolute;
-      text-align: left;
-      font-size: 20px;
-      color: gray;
-    }
-
-    .evaluation label:hover,
-    .evaluation label:hover~label,
-    .evaluation input[type='radio']:checked~label {
-      color: #ffcc00;
-    }
-
-    * {
-      box-sizing: border-box;
-    }
-
-    /* レビューポップアップ */
-    /* ウインドウ */
-    .popup {
-      background-color: #fff;
-      border-radius: 25px;
-      border-color: #cccccc;
-      box-shadow: 0 0 0 9999px rgba(255,255,255,0.5);
-      display: none;
-      position: fixed;
-      border: solid;
-      top: 10%;
-      left: 0%;
-      right: 0%;
-      bottom: 20%;
-      margin: auto;
-      width: 800px;
-      height: 900px;
-    }
-
-    /* チェックボックスの初期設定 */
-    #popup-on {
-      display: none;
-    }
-
-    /* チェックされたらポップアップウインドウを開く */
-    #popup-on:checked+.popup {
-      display: block;
-    }
-
-    /* 閉じる */
-    .icon-close {
-      background: rgba(255,255,255,0);
-      color: #808080;
-      font-size: 100px;
-      padding: 0 40px;
-      position: absolute;
-      right: 0;
-    }
-
-    /* 開く */
-    .btn-open {
-      display: block;
-      background-color: #01b6ed;
-      border-radius: 10px;
-      color: #333;
-      padding: 10px;
-      cursor: pointer;
-      margin: 10px auto;
-      width: 200px;
-      text-align: center;
-    }
-
-    /* ポップアップの内容 */
-    .popup-content {
-      margin: 40px auto 40px auto;
-      width: 90%;
-      font-size: 40px;
-    }
-    
-    input#btn_send{
-      background-color: #1a1aff;
-      border-style: none;
-      font-size: 30pt;
-      color: #fff;
-      width: 600px;
-      height: 80px;
-    }
-
-    textarea#review{
-      width: 600px;
-      height: 450px;
-      font-size: 35px;
-    }
-  </style>
 
   <!-- 送信フォーム -->
   <!-- レビューを書くボタンを押す -->
