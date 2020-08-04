06ff9e8b3ac8a7ce3525d513c2597c07da19cf9d
okuguchihonoka <e1607@s.akashi.ac.jp>
2020-08-04 16:49:16 +0900

    ポップアップ出た

--- sold.php
+++ sold.php
@@ -1,35 +1,62 @@
 <html>
-<head><title>sold</title></head>
-<body>
-  <form>
-      売り切れ情報を入力してください<br>
-      <input type="radio" name="button" value=false checked="checked">販売中
-      <br>
-      <input type="radio" name="button" value=true>売り切れ
-      <br>
-      <input type="submit" value="送信">
-  </form>
 
-<?php
-$disp_enc="UTF-8";
-$selmenu="カレーライス";
-$conn = pg_connect("host=localhost dbname=team3db user=team3 password=1qazxsw23edc") or die("error1");
-$value = $_GET['button'];
-$sql = "update menu set sold=$value where menu='$selmenu'";
-$res = pg_query($conn,$sql) or die("error2");
-$sql2 = "select sold from menu where menu='$selmenu'";
-$res2 = pg_query($conn,$sql2) or die("error3");
-$x = pg_fetch_array($res2,0,PGSQL_ASSOC);
-echo $x["sold"];
-if($x["sold"]=='t'){
-  $cross='image/r_cross.jpeg';
-}else{
-  $cross='image/g_cross.jpeg';
-}
-echo $cross;
-$pg_close($conn);
-?>
+<head>
+  <link rel="stylesheet" type="text/css" href="sold.css" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="user-scalable=no" />
+  <title>sold</title>
+</head>
+
+<body>
+  <!-- 送信フォーム -->
+  <!-- レビューを書くボタンを押す -->
+  <div class="pop-box">
+    <label for="popup-on">
+      <div class="btn-open">
+        <span>売り切れ情報</span>
+        <!--ボタン-->
+    </label>
+    <input type="checkbox" id="popup-on">
+
+    <!-- 閉じる -->
+    <div class="popup">
+      <label for="popup-on" class="icon-close">×</label>
+
+      <!-- ウィンドウの中身 -->
+      <div class="popup-content">
+        <form>
+          <p style="margin-bottom: 1em;">
+            売切れ情報投稿<br>
+            <u>カレーライス</u><br>
+            <p style="margin-bottom: 1em;"> 
+              <input type="radio" name="button" value=false checked="checked">販売中
+              <br>
+              <input type="radio" name="button" value=true>売切れ
+              <br>
+              <input id="btn_send" type="submit" value="送信">
+        </form>
+
+        <?php
+        $disp_enc = "UTF-8";
+        $selmenu = "カレーライス";
+        $conn = pg_connect("host=localhost dbname=team3db user=team3 password=1qazxsw23edc") or die("error1");
+        $value = $_GET['button'];
+        $sql = "update menu set sold=$value where menu='$selmenu'";
+        $res = pg_query($conn, $sql) or die("error2");
+        $sql2 = "select sold from menu where menu='$selmenu'";
+        $res2 = pg_query($conn, $sql2) or die("error3");
+        $x = pg_fetch_array($res2, 0, PGSQL_ASSOC);
+        //echo $x["sold"];
+        if ($x["sold"] == 't') {
+          $cross = 'image/r_cross.jpeg';
+        } else {
+          $cross = 'image/g_cross.jpeg';
+        }
+        //echo $cross;
+        $pg_close($conn);
+        ?>
 
 
 </body>
+
 </html>
