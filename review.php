e159ef3b8e2f72ef19cbeb793d623f7a06fbda11
okuguchihonoka <e1607@s.akashi.ac.jp>
2020-08-05 19:31:49 +0900

    フォーム単体にした

--- review.php
+++ review.php
@@ -14,7 +14,7 @@
   <div class="pop-box">
     <label for="popup-on">
       <div class="btn-open">
-        <span>レビューを書く</span> <!--ボタン-->
+        <span>レビューを書く</span>
     </label>
     <input type="checkbox" id="popup-on">
 
@@ -24,7 +24,7 @@
 
       <!-- ウィンドウの中身 -->
       <div class="popup-content">
-        <form action="review.php" method="POST">
+        <form action="TopPage.php" method="post">
           <p style="margin-bottom: 1em;">
           <p align= "left"><u>たこ焼き</u></p>
           <p style = "margin-bottom:1em;">
@@ -50,36 +50,6 @@
       </form>
     </div>
   </div>
-
-  <?php
-  $display_enc = "UTF-8";
-
-  // データベース接続設定
-  $sv = "localhost";
-  $name = "team3db";
-  $user = "team3";
-  $pass = "1qazxsw23edc";
-
-  // データベースに接続
-  $dbconn = pg_connect("host = $sv dbname = $name user = $user password = $pass") or die("接続エラー");
-
-  // レビューが送信されたとき
-  if (isset($_POST['send'])) {
-    $today = date('Y-m-d');
-    $menu = 'たこ焼き';
-    $star = $_POST['star'];
-
-    // コメントのみのとき
-    if ($star == null) {
-      $star = 0;
-    }
-
-    $comment = $_POST["comment"];
-    $sql = "insert into review values('$menu',$star,'$comment','$today')";
-    $res = pg_query($dbconn, $sql) or die("データ読み込みエラー");
-  }
-
-  ?>
 
 </body>
 
