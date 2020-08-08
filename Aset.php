e272270bbec42eccd43a9300a695fdef95c60534
okuguchihonoka <e1607@s.akashi.ac.jp>
2020-08-08 13:54:06 +0900

    現段階

--- Aset.php
+++ Aset.php
@@ -6,38 +6,52 @@
     <meta name="viewport" devicewidth="width=640,initial-scale=1.0,minimum-scale=1.0,user-scalable=no">
     <title>明石高専学生食堂システム/Aセット-詳細ページ</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
+
     <script language=”javascript”>resizeTo(640,1136);</script>
   </head>
 
   <body>
     <!--ヘッダー設定-->
     <header class="site-header">
-    <button class="logo" name="logo" onclick="location.href='./TopPage.html'"><img src="DesignImage/logo.jpeg"></button>
-    <button class="konzatsu"><img src="DesignImage/混雑3.jpeg" name="konzatsu"></button>
+    <button class="logo" name="logo" onclick="location.href='./TopPage.php'"><img src="/team3/DesignImage/logo.png"></button>
+    <button class="konzatsu"><img src="/team3/DesignImage/level3.png" name="konzatsu"></button>
     </header>
 
     <!--サブページ設定-->
+
+    <!--php-->
+   <?php include 'today.php';?>
+   <?php include 'review.php';?>
+
+
     <!--menuImage-->
-    <div class="MenuImage" style="text-align:center"><img src="イメージ/Aセット.png" alt="menu"></div>
+    <div class="MenuImage" style="text-align:center"><img src="/team3/DesignImage/Aset.png" alt="menu"></div>
+
+    <!--メニュー名-->
+    <h5><?php echo $dayA["menu"];?></h5>
+    <br>
+    <!--価格・カロリー情報-->
+    <h6>¥<?php echo $dayA["price"];?></h6><h7>カロリー：<?php echo $dayA["cal"];?>kcal</h7>
 
     <!--レビュー吹き出し-->
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewA["eval"];?><?php echo $reviewA["body"];?></p>
     </div>
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewA["eval"];?><?php echo $reviewA["body"];?></p>
     </div>
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewA["eval"];?><?php echo $reviewA["body"];?></p>
     </div>
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewA["eval"];?><?php echo $reviewA["body"];?></p>
     </div>
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewA["eval"];?><?php echo $reviewA["body"];?></p>
     </div>
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewA["eval"];?><?php echo $reviewA["body"];?></p>
     </div>
 
 
@@ -77,7 +91,7 @@
           <p style="margin-bottom: 2em;">
 
           <textarea id= "review"name="comment" placeholder="コメントを入力してください"></textarea>
-          <input id="btn_send" type="submit" name="send" value="送信">
+          <input id="btn_send" type="submit" name="send" value="送信" formaction="TopPage.php"/>
       </div>
       </form>
     </div>
