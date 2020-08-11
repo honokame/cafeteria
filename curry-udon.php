3f34f741a992caf88ac26916f5cf892b2ac3bb1b
okuguchihonoka <e1607@s.akashi.ac.jp>
2020-08-11 19:07:25 +0900

    php仕様

--- curry-udon.php
+++ curry-udon.php
@@ -1,55 +1,64 @@
 <!DOCTYPE html>
 <html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <!--ページ設定-->
+    <meta name="viewport" devicewidth="width=640,initial-scale=1.0,minimum-scale=1.0,user-scalable=no">
+    <title>明石高専学生食堂システム/カレーうどん-詳細ページ</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
 
-<head>
-  <meta charset="UTF-8">
-  <!--ページ設定-->
-  <meta name="viewport" devicewidth="width=640,initial-scale=1.0,minimum-scale=1.0,user-scalable=no">
-  <meta name="viewport" content="user-scalable=no" />
-  <title>明石高専学生食堂システム/カレーうどん-詳細ページ</title>
-  <link rel="stylesheet" href="style.css">
-  <script language=”javascript”>
-    resizeTo(640, 1136);
-  </script>
-</head>
+    <script language=”javascript”>resizeTo(640,1136);</script>
+  </head>
 
-<body>
-  <!--ヘッダー設定-->
-  <header class="site-header">
-    <button class="logo" name="logo" onclick="location.href='./TopPage.html'"><img src="DesignImage/logo.jpeg"></button>
-    <button class="konzatsu"><img src="DesignImage/混雑3.jpeg" name="konzatsu"></button>
-  </header>
+  <body>
+    <!--ヘッダー設定-->
+    <header class="site-header">
+    <button class="logo" name="logo" onclick="location.href='./TopPage.php'"><img src="/team3/DesignImage/logo.png"></button>
+    <button class="konzatsu"><img src="/team3/DesignImage/level3.png" name="konzatsu"></button>
+    </header>
 
-  <!--サブページ設定-->
-  <!--menuImage-->
-  <div class="MenuImage" style="text-align:center"><img src="イメージ/カレーうどん.png" alt="menu"></div>
+    <!--サブページ設定-->
 
-  <!--レビュー吹き出し-->
-  <div class="box25">
-    <p>ここに文章</p>
-  </div>
-  <div class="box25">
-    <p>ここに文章</p>
-  </div>
-  <div class="box25">
-    <p>ここに文章</p>
-  </div>
-  <div class="box25">
-    <p>ここに文章</p>
-  </div>
-  <div class="box25">
-    <p>ここに文章</p>
-  </div>
-  <div class="box25">
-    <p>ここに文章</p>
-  </div>
-  <!--レビュー書き込みボタン-->
-  
-  <div class="pop-box">
+    <!--php-->
+   <?php require_once 'today.php';?>
+   <?php require_once 'review_out.php';?>
+
+
+    <!--menuImage-->
+    <div class="MenuImage" style="text-align:center"><img src="/team3/DesignImage/curry-udon.png" alt="menu"></div>
+
+    <!--メニュー名-->
+    <h5><?php echo $dayN[2]["menu"];?></h5>
+    <br>
+    <!--価格・カロリー情報-->
+    <h6>¥<?php echo $dayN[2]["price"];?></h6><h7>カロリー：<?php echo $dayN[2]["cal"];?>kcal</h7>
+
+    <!--レビュー吹き出し-->
+    <div class="box25">
+      <p><?php echo $reviewC["eval"];?><?php echo $reviewC["body"];?></p>
+    </div>
+    <div class="box25">
+      <p><?php echo $reviewC["eval"];?><?php echo $reviewC["body"];?></p>
+    </div>
+    <div class="box25">
+      <p><?php echo $reviewC["eval"];?><?php echo $reviewC["body"];?></p>
+    </div>
+    <div class="box25">
+      <p><?php echo $reviewC["eval"];?><?php echo $reviewC["body"];?></p>
+    </div>
+    <div class="box25">
+      <p><?php echo $reviewC["eval"];?><?php echo $reviewC["body"];?></p>
+    </div>
+    <div class="box25">
+      <p><?php echo $reviewC["eval"];?><?php echo $reviewC["body"];?></p>
+    </div>
+
+    <!--レビュー書き込みボタン-->
+    <div class="pop-box">
     <label for="popup-on">
       <div class="btn-open">
-        <span>レビューを書く</span>
-        <!--ボタン-->
+        <span>レビューを書く</span> <!--ボタン-->
     </label>
     <input type="checkbox" id="popup-on">
 
@@ -61,36 +70,34 @@
       <div class="popup-content">
         <form action="TopPage.php" method="post">
           <p style="margin-bottom: 1em;">
-            <p align="left"><u>カレーうどん</u></p>
-            <p style="margin-bottom:1em;">
+          <p align= "left"><u>カレーライス</u></p>
+          <p style = "margin-bottom:1em;">
 
-              <!-- 星の表示 -->
-              <div class="evaluation">
-                <input id="star1" type="radio" name="star" value="5" />
-                <label for="star1">★</label>
-                <input id="star2" type="radio" name="star" value="4" />
-                <label for="star2">★</label>
-                <input id="star3" type="radio" name="star" value="3" />
-                <label for="star3">★</label>
-                <input id="star4" type="radio" name="star" value="2" />
-                <label for="star4">★</label>
-                <input id="star5" type="radio" name="star" value="1" />
-                <label for="star5">★</label>
-              </div>
-              <p style="margin-bottom: 2em;">
+          <!-- 星の表示 -->
+          <div class="evaluation">
+            <input id="star1" type="radio" name="star" value="5" />
+            <label for="star1">★</label>
+            <input id="star2" type="radio" name="star" value="4" />
+            <label for="star2">★</label>
+            <input id="star3" type="radio" name="star" value="3" />
+            <label for="star3">★</label>
+            <input id="star4" type="radio" name="star" value="2" />
+            <label for="star4">★</label>
+            <input id="star5" type="radio" name="star" value="1" />
+            <label for="star5">★</label>
+          </div>
+          <p style="margin-bottom: 2em;">
 
-                <input type="hidden" name="menu" value="カレーうどん">
-                <textarea id="review" name="comment" placeholder="コメントを入力してください"></textarea>
-                <input id="btn_send" type="submit" name="send" value="送信">
+          <input type="hidden" name="menu" value="カレーライス">
+          <textarea id= "review"name="comment" placeholder="コメントを入力してください"></textarea>
+          <input id="btn_send" type="submit" name="send" value="送信">
       </div>
       </form>
     </div>
   </div>
 
-  <!--フッター設定-->
-  <footer class="site-footer">
-    <p class="copyright">@2020 e16_team3</p>
-  </footer>
-</body>
-
-</html>
+    <!--フッター設定-->
+    <footer class="site-footer">
+      <p class="copyright">@2020 e16_team3</p>
+    </footer>
+  </body>
--- miso-ramen.php
+++ miso-ramen.php
@@ -4,48 +4,61 @@
     <meta charset="UTF-8">
     <!--ページ設定-->
     <meta name="viewport" devicewidth="width=640,initial-scale=1.0,minimum-scale=1.0,user-scalable=no">
-    <meta name="viewport" content="user-scalable=no" />
     <title>明石高専学生食堂システム/味噌ラーメン-詳細ページ</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
+
     <script language=”javascript”>resizeTo(640,1136);</script>
   </head>
 
   <body>
     <!--ヘッダー設定-->
     <header class="site-header">
-    <button class="logo" name="logo" onclick="location.href='./TopPage.html'"><img src="DesignImage/logo.jpeg"></button>
-    <button class="konzatsu"><img src="DesignImage/混雑3.jpeg" name="konzatsu"></button>
+    <button class="logo" name="logo" onclick="location.href='./TopPage.php'"><img src="/team3/DesignImage/logo.png"></button>
+    <button class="konzatsu"><img src="/team3/DesignImage/level3.png" name="konzatsu"></button>
     </header>
 
     <!--サブページ設定-->
+
+    <!--php-->
+   <?php require_once 'today.php';?>
+   <?php require_once 'review_out.php';?>
+
+
     <!--menuImage-->
-    <div class="MenuImage" style="text-align:center"><img src="イメージ/味噌ラーメン.png" alt="menu"></div>
+    <div class="MenuImage" style="text-align:center"><img src="/team3/DesignImage/miso-ramen.png" alt="menu"></div>
+
+    <!--メニュー名-->
+    <h5><?php echo $dayN[3]["menu"];?></h5>
+    <br>
+    <!--価格・カロリー情報-->
+    <h6>¥<?php echo $dayN[3]["price"];?></h6><h7>カロリー：<?php echo $dayN[3]["cal"];?>kcal</h7>
 
     <!--レビュー吹き出し-->
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewD["eval"];?><?php echo $reviewD["body"];?></p>
     </div>
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewD["eval"];?><?php echo $reviewD["body"];?></p>
     </div>
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewD["eval"];?><?php echo $reviewD["body"];?></p>
     </div>
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewD["eval"];?><?php echo $reviewD["body"];?></p>
     </div>
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewD["eval"];?><?php echo $reviewD["body"];?></p>
     </div>
     <div class="box25">
-      <p>ここに文章</p>
+      <p><?php echo $reviewD["eval"];?><?php echo $reviewD["body"];?></p>
     </div>
- <!--レビュー書き込みボタン-->
-  <div class="pop-box">
+
+    <!--レビュー書き込みボタン-->
+    <div class="pop-box">
     <label for="popup-on">
       <div class="btn-open">
-        <span>レビューを書く</span>
-        <!--ボタン-->
+        <span>レビューを書く</span> <!--ボタン-->
     </label>
     <input type="checkbox" id="popup-on">
 
@@ -57,27 +70,27 @@
       <div class="popup-content">
         <form action="TopPage.php" method="post">
           <p style="margin-bottom: 1em;">
-            <p align="left"><u>味噌ラーメン</u></p>
-            <p style="margin-bottom:1em;">
+          <p align= "left"><u>カレーライス</u></p>
+          <p style = "margin-bottom:1em;">
 
-              <!-- 星の表示 -->
-              <div class="evaluation">
-                <input id="star1" type="radio" name="star" value="5" />
-                <label for="star1">★</label>
-                <input id="star2" type="radio" name="star" value="4" />
-                <label for="star2">★</label>
-                <input id="star3" type="radio" name="star" value="3" />
-                <label for="star3">★</label>
-                <input id="star4" type="radio" name="star" value="2" />
-                <label for="star4">★</label>
-                <input id="star5" type="radio" name="star" value="1" />
-                <label for="star5">★</label>
-              </div>
-              <p style="margin-bottom: 2em;">
+          <!-- 星の表示 -->
+          <div class="evaluation">
+            <input id="star1" type="radio" name="star" value="5" />
+            <label for="star1">★</label>
+            <input id="star2" type="radio" name="star" value="4" />
+            <label for="star2">★</label>
+            <input id="star3" type="radio" name="star" value="3" />
+            <label for="star3">★</label>
+            <input id="star4" type="radio" name="star" value="2" />
+            <label for="star4">★</label>
+            <input id="star5" type="radio" name="star" value="1" />
+            <label for="star5">★</label>
+          </div>
+          <p style="margin-bottom: 2em;">
 
-                <input type="hidden" name="menu" value="味噌ラーメン">
-                <textarea id="review" name="comment" placeholder="コメントを入力してください"></textarea>
-                <input id="btn_send" type="submit" name="send" value="送信">
+          <input type="hidden" name="menu" value="カレーライス">
+          <textarea id= "review"name="comment" placeholder="コメントを入力してください"></textarea>
+          <input id="btn_send" type="submit" name="send" value="送信">
       </div>
       </form>
     </div>
@@ -88,4 +101,3 @@
       <p class="copyright">@2020 e16_team3</p>
     </footer>
   </body>
-</html>
